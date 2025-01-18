45a17b0d0d45f6d3079506b6eeedfc2985127c64
Jamoliddin Samadov <gamalsamadov@gmail.com>
2025-01-18 15:38:50 +0300

    Account create implement

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -3,9 +3,11 @@ import { Module } from '@nestjs/common'
 import { ConfigModule, ConfigService } from '@nestjs/config'
 import { GraphQLModule } from '@nestjs/graphql'
 
+import { AccountModule } from '../modules/auth/account/account.module'
+
 import { getGraphQLConfig } from './config/graphql.config'
 import { PrismaModule } from './prisma/prisma.module'
-import { RedisModule } from './redis/redis.module';
+import { RedisModule } from './redis/redis.module'
 
 @Module({
 	imports: [
@@ -20,7 +22,8 @@ import { RedisModule } from './redis/redis.module';
 			inject: [ConfigService]
 		}),
 		PrismaModule,
-		RedisModule
+		RedisModule,
+		AccountModule
 	]
 })
 export class CoreModule {}
---
+++ src/modules/auth/account/account.service.ts
@@ -0,0 +1,47 @@
+import { ConflictException, Injectable } from '@nestjs/common'
+import { hash } from 'argon2'
+
+import { PrismaService } from '@/src/core/prisma/prisma.service'
+
+import { CreateUserInput } from './inputs/create-user-input'
+
+@Injectable()
+export class AccountService {
+	public constructor(private readonly prisma: PrismaService) {}
+
+	public async findAll() {
+		const users = await this.prisma.user.findMany()
+		return users
+	}
+
+	public async create(input: CreateUserInput) {
+		const { email, username, password } = input
+
+		const isUsernameExists = await this.prisma.user.findUnique({
+			where: { username }
+		})
+
+		if (isUsernameExists) {
+			throw new ConflictException('Username already exists')
+		}
+
+		const isEmailExists = await this.prisma.user.findUnique({
+			where: { email }
+		})
+
+		if (isEmailExists) {
+			throw new ConflictException('Email already exists')
+		}
+
+		await this.prisma.user.create({
+			data: {
+				username,
+				email,
+				password: await hash(password),
+				displayName: username
+			}
+		})
+
+		return true
+	}
+}
