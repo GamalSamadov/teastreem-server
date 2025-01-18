1f0a049241a8c8ac0c5a6386efed3b736fbe4ba7
Jamoliddin Samadov <gamalsamadov@gmail.com>
2025-01-18 13:35:53 +0300

    GraphQL initial implement

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -1,6 +1,24 @@
+import { ApolloDriver } from '@nestjs/apollo'
 import { Module } from '@nestjs/common'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { GraphQLModule } from '@nestjs/graphql'
+
+import { getGraphQLConfig } from './config/graphql.config'
+import { PrismaModule } from './prisma/prisma.module'
 
 @Module({
-	imports: []
+	imports: [
+		ConfigModule.forRoot({
+			ignoreEnvFile: true,
+			isGlobal: true
+		}),
+		GraphQLModule.forRootAsync({
+			driver: ApolloDriver,
+			imports: [ConfigModule],
+			useFactory: getGraphQLConfig,
+			inject: [ConfigService]
+		}),
+		PrismaModule
+	]
 })
 export class CoreModule {}
