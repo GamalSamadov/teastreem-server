d6348f4ba7006420a56004449f754c073dd5b04a
Jamoliddin Samadov <gamalsamadov@gmail.com>
2025-01-18 14:51:50 +0300

    main file initial implement

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -5,6 +5,7 @@ import { GraphQLModule } from '@nestjs/graphql'
 
 import { getGraphQLConfig } from './config/graphql.config'
 import { PrismaModule } from './prisma/prisma.module'
+import { RedisModule } from './redis/redis.module';
 
 @Module({
 	imports: [
@@ -18,7 +19,8 @@ import { PrismaModule } from './prisma/prisma.module'
 			useFactory: getGraphQLConfig,
 			inject: [ConfigService]
 		}),
-		PrismaModule
+		PrismaModule,
+		RedisModule
 	]
 })
 export class CoreModule {}
